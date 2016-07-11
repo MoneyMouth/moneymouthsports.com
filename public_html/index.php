8379e0a8c8acef9b624d1d518c4738e1e4fa4ff9
MoneyMouth <rogird@gmail.com>
2016-07-11 12:34:56 -0400

    Minor updates

--- public_html/index.php
+++ public_html/index.php
@@ -24,7 +24,6 @@ if($environment === 'prod') {
 $kernel = new AppKernel($environment, $environment === 'dev');
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
-
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
